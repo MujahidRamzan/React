374700ca0809092483c09c1f62a34d1d712fee22
Mujahid Ramzan <mujahid.ramzan@vaimanagment.co>
2022-10-21 18:15:28 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+// import './App.css';
+import DrawerAppBar from './components/drawerAppbar';
+import FullWidthTabs from './components/services';
+function App() {
+  return (
+    <div className="App">
+      <br />
+      <DrawerAppBar />
+      <FullWidthTabs/>
+
+    </div>
+
+  );
+}
+
+export default App;
